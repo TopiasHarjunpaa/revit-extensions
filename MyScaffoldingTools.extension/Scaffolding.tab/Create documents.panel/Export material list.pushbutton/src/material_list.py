57b6a2b86e04c826291ab9a4dfc84c3a7b2b7921
Topias Harjunpää <topias.harjunpaa@gmail.com>
2024-06-03 09:38:24 +0300

    Create safety wire system calculations

--- MyScaffoldingTools.extension/Scaffolding.tab/Create documents.panel/Export material list.pushbutton/src/material_list.py
+++ MyScaffoldingTools.extension/Scaffolding.tab/Create documents.panel/Export material list.pushbutton/src/material_list.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import re
+import math
 
 def get_tarpaulin_parameters(length, width):
 	tarpaulin_length = float(length)/1000
@@ -28,6 +29,29 @@ def format_anchor_ledger_name(count, ledger_name):
 
     return formatted_name
 
+def calculate_wire_sets(length, set_40=0, set_60=0):
+	if length > 2000000: # Just to prevent memory errors. Input shouldn't be this high anyways.
+		print("Desired safety wire length is more than 2 kilometers. Only 60 m sets are used.")
+		return set_40, int(math.ceil(float(length) / 600000))
+	
+	if length <= 0:
+		return set_40, set_60
+	
+	if length > 80000:
+		return calculate_wire_sets(length - 60000, set_40, set_60 + 1)
+	
+	result_40 = calculate_wire_sets(length - 40000, set_40 + 1, set_60)
+	result_60 = calculate_wire_sets(length - 60000, set_40, set_60 + 1)
+
+	if sum(result_40) < sum(result_60):
+		return result_40
+	
+	elif sum(result_40) > sum(result_60):
+		return result_60
+	
+	return result_40 if result_40[0] >= result_60[0] else result_60
+
+
 def create_material_list(schedule_data, master_data, language="ENG"):
 	material_list = []
 	language_index = {"FIN": 2, "ENG": 3, "SWE": 4}[language]
@@ -35,6 +59,7 @@ def create_material_list(schedule_data, master_data, language="ENG"):
 	total_price = 0.00
 	notes = {}
 	roof_system = False
+	safety_wire_sets = (0, 0)
 
 	if len(schedule_data) == 0:
 		return material_list, notes, (total_weight, total_price)
@@ -85,6 +110,12 @@ def create_material_list(schedule_data, master_data, language="ENG"):
 			formatted_note = format_anchor_ledger_name(count, product[language_index])
 			notes.setdefault("Anchoring", []).append(formatted_note)
 			found = True # Do not add this row into material list
+		
+		if product_number == "VALJAS":
+			wire_set_length = float(product[6])
+			number_of_40_sets, number_of_60_sets = calculate_wire_sets(wire_set_length)
+			safety_wire_sets = (safety_wire_sets[0] + count * number_of_40_sets, safety_wire_sets[1] + count * number_of_60_sets)
+			found = True # Do not add this row into material list
 
 		if not found:
 			row = [count, product_number, name_fin, name_eng, name_swe, weight, 0.00]
@@ -95,6 +126,28 @@ def create_material_list(schedule_data, master_data, language="ENG"):
 		row = [0, "KHPÄÄT", "Muista lisätä päätypeitteet", "REMEMBER GABLE TARPAULINS", "Komma ihåg gavelduk", 0.00, 0.00]
 		sorted_row = sort_rows(language_index, row)
 		material_list.append(sorted_row)
+	
+	if safety_wire_sets != (0, 0):
+		number_of_40_sets, number_of_60_sets = safety_wire_sets
+
+		# At the moment these are just fixed values to get rough estimates.
+		# Later on, read the values from master list.
+		weight_40 = 25
+		weight_60 = 35
+		price_40 = 1500
+		price_60 = 2000
+		total_weight += number_of_40_sets * weight_40 + number_of_60_sets + weight_60
+		total_price += number_of_40_sets * price_40 + number_of_60_sets * price_60
+
+		if number_of_40_sets > 0:
+			row = [number_of_40_sets, product_number + "40", name_fin + " 40 m", name_eng + " 40 M", name_swe + " 40 m", weight_40, price_40]
+			sorted_row = sort_rows(language_index, row)
+			material_list.append(sorted_row)
+
+		if number_of_60_sets > 0:
+			row = [number_of_60_sets, product_number + "60", name_fin + " 60 m", name_eng + " 60 M", name_swe + " 60 m", weight_60, price_60]
+			sorted_row = sort_rows(language_index, row)
+			material_list.append(sorted_row)		
 
 	return material_list, notes, (total_weight, total_price)
 
