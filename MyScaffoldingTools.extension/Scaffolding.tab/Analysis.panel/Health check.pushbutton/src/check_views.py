0f95d252fb621af3e3406f4b4085d39bdad29be1
Topias Harjunpää <topias.harjunpaa@gmail.com>
2024-07-23 14:16:44 +0300

    create material list and viewport type checks

--- MyScaffoldingTools.extension/Scaffolding.tab/Analysis.panel/Health check.pushbutton/src/check_views.py
+++ MyScaffoldingTools.extension/Scaffolding.tab/Analysis.panel/Health check.pushbutton/src/check_views.py
@@ -3,21 +3,26 @@
 import re
 from pyrevit import revit, DB
 
-def get_project_views():
+def get_project_views_and_viewports():
     views = {}
     views_in_viewport = []
+    viewport_types = {}
 
     view_collector = DB.FilteredElementCollector(revit.doc).OfClass(DB.View)
     viewport_collector = DB.FilteredElementCollector(revit.doc).OfClass(DB.Viewport)
     
+    # NOTE: This could be refactored. No need separate list for views in viewport.
     for viewport in viewport_collector:
-        views_in_viewport.append(viewport.ViewId)
+        viewport_id = viewport.ViewId
+        viewport_type = revit.doc.GetElement(viewport.GetTypeId()).get_Parameter(DB.BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString()
+        views_in_viewport.append(viewport_id)
+        viewport_types[viewport_id] = viewport_type
 
     for view in view_collector:
         view_type = str(view.ViewType)
         view_id = view.Id
 
-        if not view.IsTemplate and view.CanBePrinted:
+        if (not view.IsTemplate and view.CanBePrinted) or view_type == "Schedule":
             if view_type not in views:
                 views[view_type] = []
             
@@ -26,7 +31,7 @@ def get_project_views():
             else:
                 views[view_type].append((view, False))
 
-    return views
+    return views, viewport_types
 
 def check_section_names_and_get_sections_in_viewports(views):
     sections_in_viewports = []
@@ -37,13 +42,25 @@ def check_section_names_and_get_sections_in_viewports(views):
         points += 1
         for section in views["Section"]:
             section_name = section[0].Name
+            print("---")
+            print(section_name)
+            print(section[0].IsCallout)
+            print("---")
 
-            if re.match(r'^[A-Z1-9]$', section_name):
-                points += 1
-                print("Section name: Correct name [{}].".format(section_name))
+            if section[0].IsCallout:
+                if re.match(r"^DET([1-9]|[1-9][0-9])$", section_name):
+                    points += 1
+                    print("Section name: Correct name [{}].".format(section_name))
+                else:
+                    print("Section name: Incorrect name [{}]. Callout section should be named as DET[number] whereas number is value between 1 - 99.".format(section_name))
+                checks += 1                
             else:
-                print("Section name: Incorrect name [{}]. Use only single character and capitalized alphanumberic, such as A, B, 1 or 2.".format(section_name))
-            checks += 1
+                if re.match(r"^[A-Z1-9]$", section_name):
+                    points += 1
+                    print("Section name: Correct name [{}].".format(section_name))
+                else:
+                    print("Section name: Incorrect name [{}]. Use only single character and capitalized alphanumberic, such as A, B, 1 or 2.".format(section_name))
+                checks += 1
 
             if section[1]:
                 sections_in_viewports.append(section[0])
@@ -100,7 +117,113 @@ def check_annotations_used_in_sections(sections_in_viewports):
         elif not contains_base_plate_families:
             print("Leg tags in section [{}]: OK. Section does not have any base plates.".format(section.Name))
         else:
-            print("Leg tags in section [{}]: OK".format(section.Name))                  
+            print("Leg tags in section [{}]: OK".format(section.Name))
+
+    return points, checks
+
+def check_material_list_headers(schedule, headers, num_of_cols):
+    if len(headers) == num_of_cols:
+        for col_index in range(num_of_cols):
+            col_name = schedule.GetCellText(DB.SectionType.Body, 0, col_index)
+            if headers[col_index] != col_name:
+                print("Material list headers: Incorrect headers. Column {0} is named as {1}. Should be {2}".format(col_index + 1, col_name, headers[col_index]))
+                return 0
+    print("Material list headers: OK")
+    return 1
+
+def check_material_list_column_cells(schedule, headers, col_index, num_of_rows):
+    for row_index in range(1, num_of_rows):
+        cell_value = schedule.GetCellText(DB.SectionType.Body, row_index, col_index)
+        
+        if cell_value == "<varies>":
+            print("{0} column: Non-unique value from the the row number {1}".format(headers[col_index], row_index))
+            return 0
+
+        if cell_value == "":
+            print("{0} column: Empty value from the the row number {1}".format(headers[col_index], row_index))
+            return 0
+    
+         
+    print("{} column: OK".format(headers[col_index]))
+    return 1
+
+def check_material_list_content(schedule):
+    points = 0
+    checks = 0
+    headers = ["Count", "Product number", "Product name (FIN)", "Product name (ENG)", "Product name (SWE)", "Weight", "Length info", "Width info"]
+    
+    table_data = schedule.GetTableData()
+    section_data = table_data.GetSectionData(DB.SectionType.Body)
+    num_of_rows = section_data.NumberOfRows
+    num_of_cols = section_data.NumberOfColumns
+
+    points += check_material_list_headers(schedule, headers, num_of_cols)
+    checks += 1
+
+    for col_index in range(1, 6):
+        points += check_material_list_column_cells(schedule, headers, col_index, num_of_rows)
+        checks += 1
+    
+    return points, checks
+
+def check_schedule_names_and_get_material_schedule(views):
+    points = 0
+    checks = 0
+    material_list = None
+
+    for schedule in views.get("Schedule", []):
+        schedule_view = schedule[0]
+        if schedule_view.Name == "Material list":
+            material_list = schedule_view
+    
+    if material_list:
+        points += 1
+        print("Material list schedule name: OK.")
+
+        results = check_material_list_content(material_list)
+        points += results[0]
+        checks += results[1]
+    else:
+        print("Material list schedule name: Incorrect. No schedule named as [Material list] found.")
+    checks += 1
+
+    return points, checks
+
+def check_viewports_have_correct_types(views, viewport_types):
+    def check_correct_types(types, correct_type, view_type):
+        if len(types) == 0:
+            print("{0} viewport types: No {0} viewports are found on the project. At least one should be used.".format(view_type))
+            return 0
+        
+        correct_types = True
+        for type in types:
+            viewport_name = type[0].Name
+            viewport_type = type[1]
+
+            if viewport_type != correct_type:
+                print("{0} viewport types: Incorrect type for {1}. Type should be {2}".format(view_type, viewport_name, correct_type))
+                correct_types = False
+        
+        if correct_types:
+            print("{} viewport types: OK.".format(view_type))
+            return 1
+        
+        return 0
+        
+    points = 0
+    checks = 0
+    floor_plan_types = [(view[0], viewport_types[view[0].Id]) for view in views.get("FloorPlan", []) if view[1]]
+    section_types = [(view[0], viewport_types[view[0].Id]) for view in views.get("Section", []) if view[1] and not view[0].IsCallout]
+    section_callout_types = [(view[0], viewport_types[view[0].Id]) for view in views.get("Section", []) if view[1] and view[0].IsCallout]
+
+    points += check_correct_types(floor_plan_types, "Title basic", "Layout")
+    checks += 1
+
+    points += check_correct_types(section_types, "Title section", "Section")
+    checks += 1
+
+    points += check_correct_types(section_callout_types, "Title basic", "Section callout")
+    checks += 1
 
     return points, checks
 
@@ -108,12 +231,20 @@ def check_views():
     view_points = 0
     view_checks = 0
 
-    views = get_project_views()
+    views, viewport_types = get_project_views_and_viewports()
     sections_in_viewports, points, checks = check_section_names_and_get_sections_in_viewports(views)
     view_points += points
     view_checks += checks
 
     points, checks = check_annotations_used_in_sections(sections_in_viewports)
+    view_points += points
+    view_checks += checks
+
+    points, checks = check_schedule_names_and_get_material_schedule(views)
+    view_points += points
+    view_checks += checks
+
+    points, checks = check_viewports_have_correct_types(views, viewport_types)
     view_points += points
     view_checks += checks
 
